c9c9aa03427a357e2f9d64ae77df6cec1b6ea88a
Emina Maric <me.mina@gmail.com>
2024-06-11 16:24:48 -0600

    Adding spectra

--- example/ge1p5_downwind.jl
+++ example/ge1p5_downwind.jl
@@ -12,40 +12,45 @@ using StaticArrays
 using AcousticMetrics
 using Statistics
 
+#EM: Adding this package for spectra calculations
+using DSP: fft, fftshift, hanning, hamming, Butterworth, filt, digitalfilter, welch_pgram
 
 function doit()
 
+    # EM: Make sure all of these values are correct with Pietro
     num_blades = 3  # number of blades
-    Rhub = 1.  # meters, fake hub
+    Rhub = 1.0  # meters, fake hub
     Rtip = 38.5  # meters
-    radii = [0, 2.3576, 5.8663, 8.1488, 10.3908, 
-        12.5901, 14.7441, 16.85, 18.9047, 20.9043, 
-        22.8447, 24.7209, 26.5271, 28.2561, 29.8994, 
-        31.4461, 32.8823, 34.1892, 35.3404, 36.2952, 
-        36.9837, 37.25	] .+ Rhub
+    radii = [0, 2.3576, 5.8663, 8.1488, 10.3908,
+        12.5901, 14.7441, 16.85, 18.9047, 20.9043,
+        22.8447, 24.7209, 26.5271, 28.2561, 29.8994,
+        31.4461, 32.8823, 34.1892, 35.3404, 36.2952,
+        36.9837, 37.25] .+ Rhub
 
     # Each of the num_blades blades have the same radial coordinates in 
     # the blade-fixed frame, but different angular coordinates: blade number 2
-    #  will be offset 180° from blade number 1.
-    θs = 2*pi/num_blades.*(0:(num_blades-1))
+    # will be offset 180° from blade number 1.
+    θs = 2 * pi / num_blades .* (0:(num_blades-1))
 
     # blade element's length from the element centers and the hub and tip location
     # DJI: this `get_dradii` function assumes:
     # DJI;   1. that the blade element interfaces are midway between each radial station defined by the `radii` vector.
     # DJI:   2. that the inner interface of the first element is at `Rhub`
-    # DJI:   3. thta the outer interface of the last element is at `Rtip`.
+    # DJI:   3. that the outer interface of the last element is at `Rtip`.
     # DJI: (the `get_dradii` function is very simple, only a few lines: https://github.com/OpenMDAO/AcousticAnalogies.jl/blob/57c199bc35cd0db6d13f17831ec34e5f1d8a9eea/src/utils.jl#L10
     # DJI: But here, it looks like radii[1] == Rhub, which makes the elements near the hub look a little goofy.
     # DJI: using the `get_dradii` routine is optional---it just returns a vector of the radial spacing of each blade element, but if you know that already, you can just use that.
     dradii = get_dradii(radii, Rhub, Rtip)
+    # EM: Make sure this is correct with Pietro
 
     # cross-sectional area of each element
+    # EM: Check values with Pietro
     cs_area_over_chord_squared = 0.064
 
-    chord = [2., 2., 3., 3., 3., 3., 
-        3., 3., 2., 2., 2., 2., 
-        2., 1., 1., 1., 1., 1., 1., 
-        1., 0.2, 0.1] # fake_chord
+    chord = [2.0, 2.0, 3.0, 3.0, 3.0, 3.0,
+        3.0, 3.0, 2.0, 2.0, 2.0, 2.0,
+        2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 0.2, 0.1] # fake_chord 
 
 
     fig = Figure()
@@ -54,9 +59,7 @@ function doit()
     hidexdecorations!(ax1, grid=false)
     save(joinpath(@__DIR__, "chord.png"), fig)
 
-
-
-    cs_area = cs_area_over_chord_squared.*chord.^2
+    cs_area = cs_area_over_chord_squared .* chord .^ 2
 
     # load openfast output
     # Import the DelimitedFiles module
@@ -112,13 +115,13 @@ function doit()
     id_b3_Ft = findfirst(x -> x == "AB3N001Ft", headers)
     id_rot_speed = findfirst(x -> x == "RotSpeed", headers)
     n_elems = length(radii)
-    Fn_b1 = data[id_b1_Fn:id_b1_Fn+n_elems-1,:]
-    Ft_b1 = data[id_b1_Ft:id_b1_Ft+n_elems-1,:]
-    Fn_b2 = data[id_b2_Fn:id_b2_Fn+n_elems-1,:]
-    Ft_b2 = data[id_b2_Ft:id_b2_Ft+n_elems-1,:]
-    Fn_b3 = data[id_b3_Fn:id_b3_Fn+n_elems-1,:]
-    Ft_b3 = data[id_b3_Ft:id_b3_Ft+n_elems-1,:]
-    omega_rpm = mean(data[id_rot_speed,:])
+    Fn_b1 = data[id_b1_Fn:id_b1_Fn+n_elems-1, :]
+    Ft_b1 = data[id_b1_Ft:id_b1_Ft+n_elems-1, :]
+    Fn_b2 = data[id_b2_Fn:id_b2_Fn+n_elems-1, :]
+    Ft_b2 = data[id_b2_Ft:id_b2_Ft+n_elems-1, :]
+    Fn_b3 = data[id_b3_Fn:id_b3_Fn+n_elems-1, :]
+    Ft_b3 = data[id_b3_Ft:id_b3_Ft+n_elems-1, :]
+    omega_rpm = mean(data[id_rot_speed, :])
 
     # DJI: are these loadings normal/tangential to the blade's rotation?
     # DJI: or are they aligned with/normal to the chord line?
@@ -128,19 +131,19 @@ function doit()
     fig = Figure()
     ax1 = fig[1, 1] = Axis(fig, xlabel="Span Position (m)", ylabel="Fn (N/m)")
     ax2 = fig[2, 1] = Axis(fig, xlabel="Span Position (m)", ylabel="Ft (N/m)")
-    l1 = lines!(ax1, radii, Fn_b1[:,1], label ="b1")
-    l1 = lines!(ax1, radii, Fn_b2[:,1], label ="b2")
-    l1 = lines!(ax1, radii, Fn_b3[:,1], label ="b3")
-    l2 = lines!(ax2, radii, Ft_b1[:,1], label ="b1")
-    l2 = lines!(ax2, radii, Ft_b2[:,1], label ="b2")
-    l2 = lines!(ax2, radii, Ft_b3[:,1], label ="b3")
+    l1 = lines!(ax1, radii, Fn_b1[:, 1], label="b1")
+    l1 = lines!(ax1, radii, Fn_b2[:, 1], label="b2")
+    l1 = lines!(ax1, radii, Fn_b3[:, 1], label="b3")
+    l2 = lines!(ax2, radii, Ft_b1[:, 1], label="b1")
+    l2 = lines!(ax2, radii, Ft_b2[:, 1], label="b2")
+    l2 = lines!(ax2, radii, Ft_b3[:, 1], label="b3")
     hidexdecorations!(ax1, grid=false)
     save(joinpath(@__DIR__, "Fn_t.png"), fig)
 
     # DJI: I was curious how unsteady the loading was, so I plotted each blade's normal and tangential loading as a function of span position for a bunch of different times.
     # DJI: times are indicated by the colorbar on the right of the plot.
     # DJI: only plotting 1 of every 500 timesteps (note the `for tidx in 1:500:ntimes_loading`)
-    @assert size(Fn_b1) == size(Fn_b2) == size(Fn_b3) == size(Ft_b1) == size(Ft_b2) == size(Ft_b3) 
+    @assert size(Fn_b1) == size(Fn_b2) == size(Fn_b3) == size(Ft_b1) == size(Ft_b2) == size(Ft_b3)
     ntimes_loading = size(Fn_b1, 2)
     fig = Figure()
     ax11 = fig[1, 1] = Axis(fig, xlabel="Span Position (m)", ylabel="Fn (N/m)", title="blade 1")
@@ -149,20 +152,19 @@ function doit()
     ax22 = fig[2, 2] = Axis(fig, xlabel="Span Position (m)", ylabel="Ft (N/m)")
     ax13 = fig[1, 3] = Axis(fig, xlabel="Span Position (m)", ylabel="Fn (N/m)", title="blade 3")
     ax23 = fig[2, 3] = Axis(fig, xlabel="Span Position (m)", ylabel="Ft (N/m)")
-    bpp = 60/omega_rpm/num_blades
+    bpp = 60 / omega_rpm / num_blades
     colormap = colorschemes[:viridis]
     for tidx in 1:500:ntimes_loading
-        cidx = (time[tidx] - time[1])/sim_length_s
-        l1 = lines!(ax11, radii, Fn_b1[:,tidx], label ="b1", color=colormap[cidx])
-        l1 = lines!(ax12, radii, Fn_b2[:,tidx], label ="b2", color=colormap[cidx])
-        l1 = lines!(ax13, radii, Fn_b3[:,tidx], label ="b3", color=colormap[cidx])
-        l2 = lines!(ax21, radii, Ft_b1[:,tidx], label ="b1", color=colormap[cidx])
-        l2 = lines!(ax22, radii, Ft_b2[:,tidx], label ="b2", color=colormap[cidx])
-        l2 = lines!(ax23, radii, Ft_b3[:,tidx], label ="b3", color=colormap[cidx])
+        cidx = (time[tidx] - time[1]) / sim_length_s
+        l1 = lines!(ax11, radii, Fn_b1[:, tidx], label="b1", color=colormap[cidx])
+        l1 = lines!(ax12, radii, Fn_b2[:, tidx], label="b2", color=colormap[cidx])
+        l1 = lines!(ax13, radii, Fn_b3[:, tidx], label="b3", color=colormap[cidx])
+        l2 = lines!(ax21, radii, Ft_b1[:, tidx], label="b1", color=colormap[cidx])
+        l2 = lines!(ax22, radii, Ft_b2[:, tidx], label="b2", color=colormap[cidx])
+        l2 = lines!(ax23, radii, Ft_b3[:, tidx], label="b3", color=colormap[cidx])
     end
 
-    cbar = Colorbar(fig[:, 4]; colormap=:viridis, limits=(0, (time[end] - time[1])/bpp), label="source time, blade passes")#, ticks=0.0:0.2:1.0)
-
+    cbar = Colorbar(fig[:, 4]; colormap=:viridis, limits=(0, (time[end] - time[1]) / bpp), label="source time, blade passes")#, ticks=0.0:0.2:1.0)
 
     linkxaxes!(ax21, ax11)
     linkxaxes!(ax21, ax11)
@@ -201,7 +203,7 @@ function doit()
     # DJI: but to do F1A correctly, we need to put all source elements in a coordinate system that moves with the fluid, i.e. one in which the fluid appears stationary.
     # So, to do that, we have the blades translating in the negative x direction.
     v = -10.0  # m/s
-    omega = omega_rpm * 2*pi/60  # rad/s
+    omega = omega_rpm * 2 * pi / 60  # rad/s
 
     # some reshaping, ses[i, j, k] holds the CompactSourceElement at src_time[i], radii[j], and blade number k
     θs = reshape(θs, 1, 1, :)
@@ -221,12 +223,12 @@ function doit()
     # orient the rotation axis of the blades as it is the global frame
     rot_axis = @SVector [1.0, 0.0, 0.0] # rotation axis aligned along global x-axis 
     blade_axis = @SVector [0.0, 0.0, 1.0]  # blade 1 pointing up, along z-axis 
-    global_trans = ConstantLinearMap(hcat(rot_axis, blade_axis, rot_axis×blade_axis))
+    global_trans = ConstantLinearMap(hcat(rot_axis, blade_axis, rot_axis × blade_axis))
 
     # blade to move with the appropriate forward velocity, and 
     # start from the desired location in the global reference frame
     y0_hub = @SVector [0.0, 0.0, 0.0]  # Position of the hub at time t0
-    v0_hub = SVector{3}(v.*rot_axis)   # Constant velocity of the hub in the global reference frame
+    v0_hub = SVector{3}(v .* rot_axis)   # Constant velocity of the hub in the global reference frame
     const_vel_trans = ConstantVelocityTransformation(t0, y0_hub, v0_hub)
 
     # combine these three transformations into one, and then use that on the SourceElements
@@ -239,7 +241,7 @@ function doit()
 
 
     # The ses object now describes how each blade element source is moving through the global reference
-    #  frame over the time src_time. As it does this, it will emit acoustics that can be sensed by an acoustic observer 
+    # frame over the time src_time. As it does this, it will emit acoustics that can be sensed by an acoustic observer 
     # (a human, or a microphone). The exact "amount" of acoustics the observer will experience depends 
     # on the relative location and motion between each source and the observer. 
     # So we'll need to define our acoustic observer before we can calculate the noise heard by it. 
@@ -271,10 +273,10 @@ function doit()
     # But we can't add them directly, yet, since the observer times are not all the same. What we need to do 
     # is first interpolate the apth of each source onto a common observer time grid, and then add them up. 
     # We'll do this using the AcousticAnalogies.combine function.
-    period = 2*pi/omega
-    bpp = period/num_blades  # blade passing period
-    obs_time_range = sim_length_s/60*omega_rpm*bpp
-    @show obs_time_range/sim_length_s # should be equalt to the 1/number of blades?
+    period = 2 * pi / omega
+    bpp = period / num_blades  # blade passing period
+    obs_time_range = sim_length_s / 60 * omega_rpm * bpp
+    @show obs_time_range / sim_length_s # should be equalt to the 1/number of blades?
     # DJI: Need to be careful to avoid extrapolation in the `combine` calculation.
     # DJI: That won't happen in this case, since obs_time_range/sim_length_s is 1/3, so the observer time range is much less than the source time range.
     # DJI: The observer time range is 1/3 of the source time range, and we're using the same number of simulation times, so that means the observer time step is 1/3 that of the source time step.
@@ -289,9 +291,10 @@ function doit()
     ax3 = fig[3, 1] = Axis(fig, xlabel="time, blade passes", ylabel="total, Pa")
     # DJI: The x axis label says the time is in units of blade passes, but I think the `time` vector here is in seconds.
     # DJI: So need to divide by the blade passing period `bpp` to get blade passes.
-    l1 = lines!(ax1, time./bpp, apth_total.p_m)
-    l2 = lines!(ax2, time./bpp, apth_total.p_d)
-    l3 = lines!(ax3, time./bpp, apth_total.p_m.+apth_total.p_d)
+    # EM: So it the time in seconds then? 
+    l1 = lines!(ax1, time ./ bpp, apth_total.p_m)
+    l2 = lines!(ax2, time ./ bpp, apth_total.p_d)
+    l3 = lines!(ax3, time ./ bpp, apth_total.p_m .+ apth_total.p_d)
     hidexdecorations!(ax1, grid=false)
     hidexdecorations!(ax2, grid=false)
     save(joinpath(@__DIR__, "openfast-apth_total.png"), fig)
@@ -313,11 +316,61 @@ function doit()
     # DJI: So we need to create the directory that will contain the VTK files ahead of time.
     mkpath(dirname(name))
     outfiles = AcousticAnalogies.to_paraview_collection(name, ses)
+
+
+    # Save data to use in Python (make sure units are correct here)
+    # Save time and total_pressure to a CSV file
+    # Total pressure is in units of [Pa] and time sould be in [seconds]
+    file_path = joinpath(@__DIR__, "julia_data.csv")
+    writedlm(file_path, hcat(time, total_pressure), ',')
+
+    ############
+    ## FFT
+    total_pressure = apth_total.p_m .+ apth_total.p_d #sum of monopole and dipole pressure 
+    @show length(total_pressure)
+    @show time
+
+    # Sampling interval assuming uniform spacing and units of seconds
+    T = time[2] - time[1]
+
+    # Parameters for Welch's method
+    exp = 12  # Example exponent for segment length
+    nperseg = 2^exp  # Segment length as power of 2
+    @show nperseg  # Show segment length
+    window = hanning(nperseg)  # Hann window
+
+    # Apply Welch's method
+    #periodogram = welch_pgram(total_pressure)
+    periodogram = welch_pgram(total_pressure, nperseg, window=window)
+
+    # Extract PSD and frequency using functions
+    psd = periodogram.power
+    freq = periodogram.freq
+
+    # Convert PSD to dB/Hz
+    pref = 20e-6
+    psd_db = 20 * log10.(abs.(psd) ./ pref)
+
+    #FFT in Pa?
+    fig = Figure()
+    ax1 = fig[1, 1] = Axis(fig, xlabel="Frequency (Hz)", ylabel="FFT [Pa]")
+    lines!(ax1, freq, psd)
+    #hidexdecorations!(ax1, grid=false)
+    save(joinpath(@__DIR__, "fft_Pa.png"), fig)
+
+    #FFT in dB?
+    fig = Figure()
+    ax1 = fig[1, 1] = Axis(fig, xlabel="Frequency (Hz)", ylabel="FFT [dB]")
+    lines!(ax1, freq, psd_db)
+    #hidexdecorations!(ax1, grid=false)
+    save(joinpath(@__DIR__, "fft_dB.png"), fig)
+
 end
 
 # DJI: When working on a script that I'm running over and over again, I like to put it in a module, include it in a Julia REPL using Revise.jl's `includet` function, then run it from the REPL via GE1p5Downwind.doit().
+# EM: In REPL run 'include("/Users/emaric/Desktop/OpenFAST_to_AA_repo/OpenFAST_to_AcousticAnalogies/example/ge1p5_downwind.jl")', then 'using .GE1p5Downwind', then 'GE1p5Downwind.doit()'
 # DJI: But if you want to manually run the script from a shell, this should do that automatically (sort of like Python's `if __name__ == "__main__":` that you see a the bottom of a lot of Python scripts.
-if ! isinteractive()
+if !isinteractive()
     doit()
 end
 
